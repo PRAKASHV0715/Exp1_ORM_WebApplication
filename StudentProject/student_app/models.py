38722e0ddc8c301cb9aa8dacc0f266b0ababfff9
PRAKASHV0715 <pv1704498@gmail.com>
2025-11-28 08:31:47 +0530

    Add Student Project Done

---
+++ StudentProject/student_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib import admin
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100, help_text="Enter the student's full name")
+    age = models.IntegerField(help_text="Enter the student's age")
+    date = models.DateField(help_text="Enter the date of enrollment")
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age', 'date']   
